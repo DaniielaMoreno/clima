8c01b5d49ea291bdd31d056b5e5b28533a8c14ec
DaniielaMoreno <daniielamoreno@gmail.com>
2025-10-12 22:48:46 +0200

    Weather Data completely updated in js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,48 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#searched-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let feelsLike = response.data.temperature.feels_like;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let pressureElement = document.querySelector("#pressure");
+  let pressure = response.data.temperature.pressure;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  feelsLikeElement.innerHTML = `${Math.round(feelsLike)}°C`;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${Math.round(wind)} km/h`;
+  pressureElement.innerHTML = `${pressure} hPa`;
+  console.log(response.data.temperature.pressure);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function getCityInfo(city) {
