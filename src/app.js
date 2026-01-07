6af04695d97f60619042ad483c35638481d18e6a
DaniielaMoreno <daniielamoreno@gmail.com>
2026-01-07 23:53:28 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -70,13 +70,13 @@ function changeIcon(description) {
 function changeImage(description) {
   let image =
     "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/174/348/original/Nublado.gif?1759348577";
-  if (icon.includes("rain")) {
+  if (description.includes("rain")) {
     image =
       "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/174/347/original/Lluvia.gif?1759348555";
-  } else if (icon.includes ("snow")) {
+  } else if (description == "snow") {
     image =
       "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/174/345/original/Nieve.gif?1759348526";
-  } else if (icon.includes ("mist")) {
+  } else if (description == "fog") {
       image =
       "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/177/211/original/Niebla.gif?1767819989";
   }  else if (description == "overcast clouds") {
